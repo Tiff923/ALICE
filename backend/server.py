946af4cc9fe7686031e67f1adfac4b8f95d8fc68
icyyci <corneliusyap@yahoo.com>
2020-06-03 00:34:18 +0800

    added upload page

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import random
+from .Sentiment import SentimentAnalysis
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -25,6 +26,22 @@ def send():
     idNumber = random.randint(10000,100000000)
     sendData = {"request": "TestReceive", "id": idNumber}
     return jsonify(sendData)
+
+@app.route("/uploadFile", methods=["GET", "POST"])
+def receiveFile():
+    file = request.files['file']
+    text = file.read()
+    text = text.decode("utf-8")
+    sentiment = SentimentAnalysis.getSentiment(text)
+    print(sentiment)
+    return f"<h1>Success</h1>"
+
+@app.route("/uploadText", methods=["GET","POST"])
+def receiveText():
+    data = request.get_json()
+    text = data['data']
+    print(text)
+    return f"<h1>Success</h1>"
 
 
 
