8033be1888a98de2b8f4e600a514d331758ab4d9
Tiffany <gohtiffany9@gmail.com>
2020-07-20 16:26:41 +0800

    Added workflow in aboutPage

--- frontend/src/components/FrontPage/About.js
+++ frontend/src/components/FrontPage/About.js
@@ -147,29 +147,14 @@ const About = (props) => {
         </div>
 
         <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-            <div id="FeaturesText" className="my-3 py-3">
-              <h2 className="display-5">Classification</h2>
-              <p className="lead">
-                Classifies documents into either of the 6 categories: Crime,
-                Tech, Health, Finance, Terrorism, Politics
-              </p>
-            </div>
-            <div className="bg-light shadow-sm mx-auto"></div>
+          <div className="stepOneBackground mr-md-3 pt-3 px-3 pt-md-5 px-md-5">
+            <h2 className='stepLabelWhite'>Upload</h2>
           </div>
-          <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div id="FeaturesText" className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-dark shadow-sm mx-auto"></div>
+          <div className="stepTwoBackground mr-md-3 pt-3 px-3 pt-md-5 px-md-5">
+            <h2 className='stepLabelBlack'>Analyze</h2>
           </div>
-          <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div id="FeaturesText" className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div className="bg-dark shadow-sm mx-auto"></div>
+          <div className="stepThreeBackground mr-md-3 pt-3 px-3 pt-md-5 px-md-5">
+            <h2 className='stepLabelBlack'>Save</h2>
           </div>
         </div>
       </div>
