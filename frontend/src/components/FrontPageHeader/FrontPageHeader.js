baf2515306323831eda2e9560aea8aa5af9b4bbd
icyyci <corneliusyap@yahoo.com>
2020-07-24 13:39:59 +0800

    add docs link to frontend

--- frontend/src/components/FrontPageHeader/FrontPageHeader.js
+++ frontend/src/components/FrontPageHeader/FrontPageHeader.js
@@ -11,6 +11,9 @@ const FrontPageHeader = () => {
         <a href="/about">
           <span className="front-page-link-text">About</span>
         </a>
+        <a href="http://docs-alice.apps.8d5714affbde4fa6828a.southeastasia.azmosa.io">
+        <span className="front-page-link-text">Docs</span>
+        </a>
         <a href="/Login">
           <span className="front-page-link-text">Login</span>
         </a>
