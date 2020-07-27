87fe43b20256f9d62d795fb9b1cfa1d6192ef38f
Tiff923 <64763976+Tiff923@users.noreply.github.com>
2020-07-27 21:50:44 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -478,6 +478,7 @@ def postABSA(data):
     try:
         url = "http://absa-alice.apps.8d5714affbde4fa6828a.southeastasia.azmosa.io/aspectSentiment"
         result = requests.post(url, json=data)
+        print('result from ABSA', result, flush=True)
         result = result.json()
     except Exception as err:
          print(f"Error in ABSA: {err}", flush=True)
