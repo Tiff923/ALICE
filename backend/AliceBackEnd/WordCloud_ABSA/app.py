7c36cccbe88c32be54cab4fd4a048006d709b63f
Tiffany <gohtiffany9@gmail.com>
2020-08-07 12:57:51 +0800

    nltk.download change

--- backend/AliceBackEnd/WordCloud_ABSA/app.py
+++ backend/AliceBackEnd/WordCloud_ABSA/app.py
@@ -10,6 +10,8 @@ from nltk.tokenize import word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask_cors import CORS, cross_origin
 
+nltk.download('punkt')
+nltk.download('vader_lexicon')
 
 app = Flask(__name__)
 cors = CORS(app)
